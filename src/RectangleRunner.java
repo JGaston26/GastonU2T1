bcff5901bdbabb43deb9d45a63cc9fd314c455d2
BT_1N3_14 <sample@bths.edu>
2023-09-28 09:16:49 -0400

    v2

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -5,26 +5,17 @@ public class RectangleRunner {
         Rectangle plot2 = new Rectangle(200);
         Rectangle plot3 = new Rectangle();
 
-        System.out.println();
-
         plot1.setWidth(75);
-        System.out.println("Plot 1 length: " + plot1.getLength());
-        System.out.println("Plot 1 width: " + plot1.getWidth());
-        System.out.println("Plot 1 area: " + plot1.calculateArea());
-
-        System.out.println();
-
         plot2.setWidth(75);
         plot2.setLength(75);
-        System.out.println("Plot 2 length: " + plot2.getLength());
-        System.out.println("Plot 2 width: " + plot2.getWidth());
-        System.out.println("Plot 2 area: " + plot2.calculateArea());
-
-        System.out.println();
-
         plot3.setWidth(75);
-        System.out.println("Plot 3 length: " + plot3.getLength());
-        System.out.println("Plot 3 width: " + plot3.getWidth());
-        System.out.println("Plot 3 area: " + plot3.calculateArea());
+
+        int plot1Area = plot1.calculateArea();
+        int plot2Area = plot2.calculateArea();
+        int plot3Area = plot3.calculateArea();
+        int totalArea = plot1Area + plot2Area + plot3Area;
+
+        System.out.println("These three plots require " + totalArea + " square feet of seed.");
+
     }
 }
